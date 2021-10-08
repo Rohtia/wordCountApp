2ad57cebfb4256f4951f48d09686488896d21250
Rohtia <90939780+Rohtia@users.noreply.github.com>
2021-10-08 18:42:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my submission for [Scrimba's](https://scrimba.com/) Weekly Web Dev Challenge. Number 43: Word Count.
+
+I only started coding on 17 Sep 21 (21 days ago), and my initial commit took me about 3 hours to write, so I'm pretty happy with that. Still, I will be doing more work on it before I submit it for the competition.
---
+++ index.js
@@ -0,0 +1,65 @@
+const textArea = document.getElementById("textArea");
+var characterCount= document.getElementById("textArea");
+var wordCount;
+var characters;
+var words;
+var l;
+var enterKeys;
+var w = 0;
+
+textArea.addEventListener("input", count);
+
+function count () {
+	const text = textArea.value;
+  //console.log(text);
+
+  characterCount = text.split(""); //turn text area into an array, count the array elements for number of characters
+  characters = countCharacters(characterCount);
+  document.getElementById("cCount").innerHTML = characters; //prints number of characters to page.
+
+  wordCount = text.split(" "); //turn text area into an array, count array elements for number of words
+  console.log(wordCount);
+ 	words = countWords(wordCount);
+  //words = words + enterKeys; //word count based on number of spaces plus number of linebreaks
+ /* console.log(words);
+  console.log(enterKeys); */
+  document.getElementById("wCount").innerHTML = words;
+}
+
+function countCharacters (count) {
+	l = count.length; //running count of characters
+
+  for (let i = 0; i < count.length; i++) {
+  //to go through each element of the array
+  	if (count[i] === " ") { //if character is a space
+    	l--; //subtract one from character count
+      console.log("empty string in countCharacters");
+    } else if (count[i] === "") {
+    	console.log("nothing in count characters");
+    }
+  }
+  return l; //returns the number of non-space characters
+} //working
+
+/* function enter(br) {
+  for (let i = 0; i < br.length; i++) {
+    //go through each element
+    if (br[i].split("/n")) { //if the sting has a line break
+      w++;
+      console.log("enter hit");
+    }
+  }
+  return w;
+}
+ */
+function countWords (count) {
+	l= count.length; //running count of words
+
+  for (let i = 0; i < count.length; i++) {
+  	if (count[i] == "") {
+    	l--;
+      console.log("empty array in countWords");
+    }
+	}
+  return l;
+ } //working
